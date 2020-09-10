7f08753464c5542e6d5fa0342bb26ed71589d715
Aspirin2d <aspirin2d@outlook.com>
2020-09-10 20:32:52 +0800

    add more code covers

--- player_test.go
+++ player_test.go
@@ -8,8 +8,18 @@ import (
 
 func TestNewSkill(t *testing.T) {
 	s, err := NewSkill(101, 102, 253, "Hello")
+
 	assert.Nil(t, err)
 	assert.Equal(t, 101, s.Attribute())
 	assert.Equal(t, 102, s.ID())
 	assert.Equal(t, 253, s.Level())
+
+	s, err = NewSkill(101, 102, 256, "Hello")
+	assert.NotNil(t, err)
+
+	s, err = NewSkill(101, 300, 2, "Hello")
+	assert.NotNil(t, err)
+
+	s, err = NewSkill(301, 2, 2, "Hello")
+	assert.NotNil(t, err)
 }
