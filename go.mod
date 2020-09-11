c0caab125d1a2534004018c5f012dede5a4e3248
Aspirin2d <aspirin2d@outlook.com>
2020-09-11 16:26:42 +0800

    add toml parser

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sleep2death/daft
 go 1.14
 
 require (
+	github.com/pelletier/go-toml v1.8.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1
 )
--- player_test.go
+++ player_test.go
@@ -1,6 +1,7 @@
 package daft
 
 import (
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +11,7 @@ func TestNewSkill(t *testing.T) {
 	s, err := NewSkill(101, 102, 253, "Hello")
 
 	assert.Nil(t, err)
+
 	assert.Equal(t, 101, s.Attribute())
 	assert.Equal(t, 102, s.ID())
 	assert.Equal(t, 253, s.Level())
@@ -22,4 +24,15 @@ func TestNewSkill(t *testing.T) {
 
 	s, err = NewSkill(301, 2, 2, "Hello")
 	assert.NotNil(t, err)
+}
+
+func TestNewConfigFromToml(t *testing.T) {
+	src, err := ioutil.ReadFile("./toml/player.toml")
+	assert.Nil(t, err)
+
+	c, err := NewConfigFromToml(src)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 1, len(c.Players))
+	assert.Equal(t, 4, len(c.Players[0].Attributes))
 }
